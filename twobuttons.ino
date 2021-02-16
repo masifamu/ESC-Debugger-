866da01dc87aab780c87f953de4c50ab43372d1f
Mohammad Asif <69226688+masifamu@users.noreply.github.com>
2021-02-16 19:01:15 +0530

    almost finalized, able to write and read data from internal eeprom

--- twobuttons.ino
+++ twobuttons.ino
@@ -2,6 +2,7 @@
 #include <SPI.h>
 #include <Adafruit_GFX.h>           //Includes core graphics library
 #include <Adafruit_SSD1306.h>
+#include <EEPROM.h>
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
@@ -27,25 +28,21 @@ OLEDState oledDisplayState = MainMenu;
 //float batt3cellVolt=0;
 
 int sensor_pin = 2; // IR sensor connection
-int count=0;
+unsigned int count=0;
+unsigned int rpm=0; // RPM value
+uint32_t timeBase=0,elapsedTime=0,prevTime=0, timePresent=0,totalTime=0,lastTotalTime=0;
 
+float distance=0.0f,totalPower=0.0f;
+float wheel_dia=0.6096f;//in meters
 
-const int dataIN = 2; //IR sensor INPUT
-
-unsigned long prevmillis; // To store time
-unsigned long duration; // To store time difference
-int rpm; // RPM value
-
-boolean currentstate; // Current state of IR input scan
-boolean prevstate; // State of IR sensor in previous scan
-
+bool addressOverflown=false;
 
 void setup() {
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
-  Serial.println("Starting TwoButtons...");
+  Serial.println("Trip Analysis Module");
 
   // link the button 1 functions.
   mode.attachClick(click1);
@@ -68,11 +65,10 @@ void setup() {
   pinMode(13, OUTPUT);
   pinMode(3,OUTPUT);
   pinMode(4,OUTPUT);
+  PORTD &= ~(1<<PORTD4);
   
-  pinMode(dataIN,INPUT); 
-  prevmillis = 0;
-  prevstate = LOW;
-  
+  pinMode(sensor_pin,INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(sensor_pin), counter, FALLING);
 } // setup
 
 
@@ -90,21 +86,72 @@ void loop() {
     temp = (((analogRead(A2)*5.0f)/1024.0f)-2.51f);
     current= temp < 0?(uint8_t)0:(uint8_t)(temp*15.15151515f);
 
-     // Measure Actual RPM 
+    // Measure Actual RPM
+    static uint32_t filterElapsedTime;
+    filterElapsedTime = ((filterElapsedTime<<3)-filterElapsedTime+elapsedTime)>>3;
+    rpm = 60000/filterElapsedTime;
 
-  Serial.println(rpm);
-  
-    dbgMenu(batt3cellVolt,batt48Volt,current,rpm);
+    totalPower += (filterElapsedTime*batt48Volt*current)/60.0f/60.0f/1000.0f;//to get WH
+
+    totalTime = timeBase/100;
+    dbgMenu(batt3cellVolt,batt48Volt,current,rpm,totalTime);
+    
+    if(totalTime % 2 == 0 && lastTotalTime != totalTime && totalTime > 10){//after 10sec starts storing data
+      lastTotalTime = totalTime;
+      if(!addressOverflown){
+        static int address;
+        storeInt(address,rpm);
+        address = address + 2;
+        storeInt(address,batt48Volt);
+        address = address + 2;
+        storeInt(address,current);
+        address = address + 2;
+        storeLastAdd(address);
+        //checking if the EEPROM is full: FULL: green LED:ON
+        if (address == (EEPROM.length()-8)) {
+          PIND |= (1<<PIND4);
+          addressOverflown = true;
+        }
+        //Serial.println(address);
+      }
+    }
+    //store distance, time, totalP
+    storeInt(1017,(int)distance);
+    storeInt(1019,totalTime);
+    storeInt(1021,(int)totalPower);
   }else if(oledDisplayState == CfgMenu){
     cfgMenu();
   }else if(oledDisplayState == LEDMenu){
     ledMenu(20);
-    blinkLEDs();
+
+    //reading EEPROM values
+    static int add;
+    while(add < readLastAdd()){
+      Serial.print(readInt(add));Serial.print(" ");
+      add += 2;
+      if(add %6 == 0) Serial.println(" ");
+    }
+    if(add == readLastAdd()){
+      Serial.print("Distance: ");Serial.print(readInt(1017));Serial.println(" m");
+      Serial.print("Time: ");Serial.print(readInt(1019));Serial.println(" s");
+      Serial.print("totalPower: ");Serial.print(readInt(1021));Serial.println(" wh");
+      add += 2;
+    }
   }
   
   delay(10);
 } // loop
 
+void counter() {
+  count++;
+  PORTD ^= (1<<PORTD3);
+  
+  timePresent = millis();
+  elapsedTime = timePresent-prevTime;
+  prevTime = timePresent;
+
+  distance=distance+3.14f*wheel_dia;
+}
 
 // ----- button 1 callback functions
 
@@ -165,7 +212,7 @@ void OLEDMenu(){
   display.setCursor(0,0);             // Start at top-left corner
   display.println(F("0:Dbg ESC"));
   display.println(F("1:Cfg ESC"));
-  display.println(F("2:LEDs"));
+  display.println(F("2:EEPROM"));
   display.setCursor(0,50);
   display.println(F("M:"));
   display.setCursor(26,50);
@@ -176,7 +223,7 @@ void OLEDMenu(){
   display.println(F("SEL"));
   display.display();
 }
-void dbgMenu(uint8_t v12, uint8_t v48,uint8_t curr, uint16_t rpm){
+void dbgMenu(uint8_t v12, uint8_t v48,uint8_t curr, uint16_t rpm, uint32_t t){
   // Clear the buffer
   display.clearDisplay();
   display.setTextSize(1);             // Normal 1:1 pixel scale
@@ -204,14 +251,15 @@ void dbgMenu(uint8_t v12, uint8_t v48,uint8_t curr, uint16_t rpm){
   display.setCursor(64,30);
   display.print(curr);
   display.println(F("A"));//battery capacity
-/*
+  
   //making third row
   //display.setTextColor(WHITE);
   display.setCursor(0,50); 
   display.setTextSize(2); 
-  display.print(rpm);
-  display.println(F("v"));//battery capacity
-
+  display.print((t/60)/60);display.print(F("h"));//hours
+  display.print((t/60)%60);display.print(F("m"));//minutes
+  display.print(t%60);display.println(F("s"));//seconds
+/*
   display.setCursor(64,50);
   display.print(rpm);
   display.println(F("v"));//battery capacity
@@ -235,13 +283,12 @@ void ledMenu(unsigned int delayTime){
   display.setTextColor(BLACK,WHITE);        // Draw white text
   display.setCursor(25,0);             // Start at top-left corner
   display.println(F("**LED Menu**"));
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.println(F("Reading"));
+  display.println(F("EEPROM"));
   display.display();
 }
-
-
-
-
-
 
 void startTimer1(){
   //set timer1 interrupt at 100Hz
@@ -258,21 +305,9 @@ void startTimer1(){
   TIMSK1 |= (1 << OCIE1A);
 }
 ISR(TIMER1_COMPA_vect){//timer1 interrupt 100Hz toggles
-  // keep watching the push buttons:
+  timeBase++;//one increment in timeBase is equal to 10ms.
   mode.tick();
   select.tick();
-
-   currentstate = digitalRead(dataIN); // Read IR sensor state
- if( prevstate != currentstate) // If there is change in input
-   {
-     if( currentstate == HIGH ) // If input changes only from LOW to HIGH
-       {
-         duration = ( micros() - prevmillis ); // Time difference between revolution in microsecond
-         prevmillis = micros(); // store time for next revolution calculation
-         rpm = (60000000/duration); // rpm = (1/ time millis)*1000*1000*60;
-       }
-   }
-  prevstate = currentstate; // store this scan (prev scan) data for next scan
 }
 
 void blinkLEDs(){
@@ -283,4 +318,25 @@ void blinkLEDs(){
   digitalWrite(4,LOW);
   delay(1000);
 }
+
+void storeLastAdd(int addVal){
+  //storing data to eeprom
+    EEPROM.update(1023, addVal);
+}
+int readLastAdd(){
+  //storing data to eeprom
+  return EEPROM.read(1023);
+}
+
+void storeInt(int baseAdd,int integer){
+  unsigned int q=0,r=0;
+  q=integer/256;
+  r=integer%256;
+  EEPROM.update(baseAdd, q);
+  EEPROM.update(baseAdd+1, r);
+}
+int readInt(int baseAdd){
+  return (EEPROM.read(baseAdd)*256+EEPROM.read(baseAdd+1));
+}
+
 // End
